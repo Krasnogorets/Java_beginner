82d60876af8d078f0dc9dd2362c449ac1dc82166
Philipp Kurpekov <p.kurpekov@gmail.com>
2023-03-02 19:09:05 +0300

    first java inro

---
+++ lection1/program.java
@@ -0,0 +1,18 @@
+
+
+public class program {
+    public static void main(String[] args) {
+      
+        int a =200;
+        a--;
+        //a=a-- - --a;
+        System.out.println(a);
+        int x =4;
+        x=--x - x--;
+        System.out.println(x);
+        int c =4;
+        c=--c-c--;
+        System.out.println(c);
+
+    }
+}
