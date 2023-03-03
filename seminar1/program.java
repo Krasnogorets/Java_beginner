4fe2533fac0a8cadf95278e7b0ee2884b6ebead0
Philipp Kurpekov <p.kurpekov@gmail.com>
2023-03-03 19:17:38 +0300

    fisrt firtst steps in java

---
+++ seminar1/program.java
@@ -0,0 +1,21 @@
+package seminar1;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+        // LocalDateTime newtime = LocalDateTime.now();
+        // System.out.println(newtime);
+        // System.out.println(LocalDateTime.now().getYear());
+        // System.out.println(newtime.withMinute(0));
+
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("твое имя , чувак: ");
+        String name = iScanner.nextLine();
+        System.out.printf("Привет, "+ name);
+        iScanner.close();
+    }
+}
